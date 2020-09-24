c445a51a7aa334f74ebe8915698698889a70739d
gargar77 <gbautista2@knights.ucf.edu>
2020-09-24 13:21:43 -0400

    created obstacle skeleton

---
+++ src/components/Obstacle/Obstacle.js
@@ -0,0 +1,19 @@
+import './Obstacle.css';
+import React, {Component} from 'react';
+
+
+class Obstacle extends Component {
+
+   
+    render() {
+
+        return (
+            <div 
+                className="obstacle"
+                    ></div>
+        )
+    }
+}
+
+
+export default Obstacle;
