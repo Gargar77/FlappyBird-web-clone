44dae70558cc5330028bfc1d6926191a99637170
gargar77 <gbautista2@knights.ucf.edu>
2020-09-24 13:44:01 -0400

    able to generate obstacle component with different orientations

--- src/components/Obstacle/Obstacle.js
+++ src/components/Obstacle/Obstacle.js
@@ -4,12 +4,21 @@ import React, {Component} from 'react';
 
 class Obstacle extends Component {
 
+    getOrientation() {
+        if (this.props.flipped) {
+            return {top:"0"}
+        } else {
+            return {bottom:"0"}
+        }
+    }
    
-    render() {
 
+    render() {
+        const orientation = this.getOrientation();
         return (
             <div 
                 className="obstacle"
+                style={orientation}
                     ></div>
         )
     }
