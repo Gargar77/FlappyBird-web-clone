7aaa5cb6387c6d83ef1c3c5e95add904dbf398e6
gargar77 <gbautista2@knights.ucf.edu>
2020-09-24 14:39:49 -0400

    obstacle can now move across the screen

--- src/components/Obstacle/Obstacle.js
+++ src/components/Obstacle/Obstacle.js
@@ -4,21 +4,55 @@ import React, {Component} from 'react';
 
 class Obstacle extends Component {
 
+    state = {
+        rightPos:-20
+    }
+
+    componentDidMount() {
+        setInterval(()=> {
+            if (this.state.rightPos <= 110) {
+                this.moveObstacle();
+            }
+        },50)
+       
+    }
+
+    moveObstacle = () => {
+        const newRightPos = this.state.rightPos + 1;
+        requestAnimationFrame(()=> {
+            this.setState({
+                ...this.state,
+                rightPos: newRightPos
+            })
+        })
+    }
+
     getOrientation() {
         if (this.props.flipped) {
-            return {top:"0"}
+            return {top:0}
         } else {
-            return {bottom:"0"}
+            return {bottom:0}
         }
+    }
+
+    getNewPos() {
+        return {right: `${this.state.rightPos}%`}
     }
    
 
     render() {
         const orientation = this.getOrientation();
+        const currentRightPos= this.getNewPos();
+
+        const activeStyle = {
+            ...orientation,
+            ...currentRightPos
+        }
+        
         return (
             <div 
                 className="obstacle"
-                style={orientation}
+                style={activeStyle}
                     ></div>
         )
     }
