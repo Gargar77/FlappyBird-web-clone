d6d3fd30d912a5ef6290837c5454ed532187a9ec
gargar77 <gbautista2@knights.ucf.edu>
2020-09-25 10:14:19 -0400

    created and positioned bird component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ObstacleGenerator from './containers/ObstacleGenerator/ObstacleGenerator';
+import Bird from './components/Bird/Bird';
 const App = () => {
     return (
       <div className="game-container">
         <div className="sky">
        <ObstacleGenerator/>
+       <Bird/>
         </div>
         <div className="ground"></div>
       </div>
---
+++ src/components/Bird/Bird.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './Bird.css';
+
+class Bird extends Component {
+    render() {
+        return <div className="Bird"></div>
+    }
+}
+
+
+export default Bird;
